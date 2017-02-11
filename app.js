fd0a8e70ed277570b780cf7094b1dfc43cdcfc25
Renate Winther Ravnaas <renatewr@gmail.com>
2017-02-11 21:37:11 +0100

    Added some js

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+const heading = document.querySelector('.jump');
+
+
+function sparanWrap(word) {
+  return [...word].map(letter => `<span>${letter}</span>`).join('');
+}
+
+heading.innerHTML = sparanWrap(heading.textContent);
